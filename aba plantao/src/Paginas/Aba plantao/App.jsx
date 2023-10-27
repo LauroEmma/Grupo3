302b5caaedfeeb40ac793d5e4e4f39c4cd39cc10
ZeL-ucas <zelucaspomps15@gmail.com>
2023-10-27 13:50:20 -0300

    aliu

---
+++ aba plantao/src/Paginas/Aba plantao/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="background">
+      <button className="botao">
+        teste
+      </button>
+      <div className="abalateral">
+      texte
+      </div>
+    </div>
+  )
+}
+
+export default App
