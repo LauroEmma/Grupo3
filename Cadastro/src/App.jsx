0a6ce9f92b58cef05892eac56e7363a5a6d70ca5
Vinícius <viny2003@gmail.com>
2023-10-27 13:47:29 -0300

    teste

---
+++ Cadastro/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <div>
+      <div className="all">
+        <div className="frame">
+          <div className="gap">
+            <input className="input"></input>
+            <input className="input"></input>
+            <input className="input"></input>
+          </div>
+            <div>
+              <button className="input"></button>
+            </div>
+          <div className="div-button">
+            <button className="button"> Cadastrar </button>
+            <button className="button"> Logue aqui !!! </button>
+          </div>
+        </div>
+        <div className="frame">
+          <div className="div-button"> 
+            <button className="button"> Logar </button>
+            <button className="button"> Cadastre-se aqui </button>
+          </div>
+          <div className="gap">
+            <input className="input"></input>
+            <input className="input"></input>
+            <input className="input"></input>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+    
+}
+
+export default App
