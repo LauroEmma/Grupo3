65c19597fb4233e86897b53ee1c93e7218d6c557
LauroEmm <lauro.rafael.ismart@gmail.com>
2023-11-02 10:06:34 -0300

    Avanço frontend tela home

---
+++ Grupo5/src/Pages/Home/Styles.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { Carousel } from "react-responsive-carousel";
+
+export const CarouselStyled = styled(Carousel)`
+  width: 600px;
+  height: 200px;
+  margin: auto;
+  margin-top: 100px;
+`;
+export const Body = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #3d8ebc;
+  border: none;
+  padding: 0;
+`;
+export const DivBackground = styled.div`
+  width: 1440px;
+  height: 1080px;
+  margin-top: 300px;
+  background-color: #3d8ebc;
+  border: none;
+`;
+
+export const BotaoPlantao = styled.button`
+  width: 202px;
+  height: 32px;
+  flex-shrink: 0;
+  background-color: #4650a7;
+  margin-top: 22px;
+  margin-left: 1122px;
+  border-radius: 20px;
+  color: #fff;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const Retangulodeinformaços = styled.div`
+  width: 1000px;
+  height: 1010px;
+  background-color: #72c7f6;
+  margin: auto;
+`;
+export const Header = styled.div`
+  width: 1300px;
+  height: 95px;
+  flex-shrink: 0;
+  background-color: #72c7f6;
+`;
+export const DivHeader = styled.div`
+  width: 100px;
+  height: 95px;
+  flex-shrink: 0;
+  background-color: #72c7f6;
+  color: #faefef;
+  font-family: Inter;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+  display: inline-block;
+`;
