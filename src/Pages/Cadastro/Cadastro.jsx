169304bb14b1f2c651b246ea87fefbc1905469d1
Vinícius <viny2003@gmail.com>
2023-11-12 12:09:56 -0300

    responsividade

--- src/Pages/Cadastro/Cadastro.jsx
+++ src/Pages/Cadastro/Cadastro.jsx
@@ -1,58 +1,38 @@
-import {
-  DivGap,
-  DivGap1,
-  DivGap2,
-  DivGap3,
-  InputC,
-  Input1,
-  Botao,
-  DivFrame,
-  Fonte1,
-  Fonte2,
-  Fonte3,
-  Divimg,
-  Divall,
-  Simbolo,
-  Doctorimg,
-} from "./Styles";
-import ImagemMedico from "../../Assets/Cadastroimg.png";
+import { DivGap, DivGap1, DivGap2, InputC, Input1, Botao, DivFrame, Fonte1, Fonte2, Fonte3, Divimg, Divall, Simbolo, Doctorimg} from "./Styles";
+import ImagemMedico from "../../Assets/Cadastroimg.png"; 
 import Cruz from "../../Assets/Cruz.png";
 import { useNavigate } from "react-router-dom";
 
-function Cadastro() {
-  const navigate = useNavigate();
-  return (
-    <Divall>
-      <DivFrame>
-        <DivGap>
-          <DivGap1>
-            <Simbolo src={Cruz} />
-            <Fonte2> Primeira vez aqui?</Fonte2>
-          </DivGap1>
-          <Fonte3>
-            {" "}
-            Faça já seu cadastro e descubra novas formas de manejar seu{" "}
-            <span> tempo de trabalho </span>{" "}
-          </Fonte3>
-          <Input1 type="email" placeholder="E-mail"></Input1>
-          <Input1 type="password" placeholder="Senha"></Input1>
-          <Input1 type="password" placeholder="Confirmar senha"></Input1>
-          <DivGap2>
-            <InputC placeholder="Cpf"></InputC>
-            <Fonte1> Já possui login? </Fonte1>
-          </DivGap2>
-          <DivGap2>
-            <Botao> Cadastrar </Botao>
-            <Botao onClick={() => navigate("/login")}> Logue aqui!!!</Botao>
-          </DivGap2>
-        </DivGap>
-        <Divimg>
-          {" "}
-          <Doctorimg src={ImagemMedico} alt="Imagem Médico" />{" "}
-        </Divimg>
-      </DivFrame>
-    </Divall>
-  );
+function Cadastro(){
+
+    const navigate = useNavigate();
+    return(
+        <Divall>
+            <DivFrame>
+                <DivGap>
+                    <DivGap1>
+                        <Simbolo src= {Cruz}/>
+                        <Fonte2> Primeira vez aqui?</Fonte2>
+                    </DivGap1>
+                    <Fonte3> Faça já seu cadastro e descubra novas formas de manejar seu <span> tempo de trabalho </span> </Fonte3>
+                    <Input1  placeholder="Nome"></Input1>
+                    <Input1  placeholder="Cargo"></Input1>
+                    <Input1 type="email" placeholder="E-mail"></Input1>
+                    <Input1 type="password" placeholder="Senha"></Input1>
+                    <Input1 type="password" placeholder="Confirmar senha"></Input1>
+                        <DivGap2>
+                            <Fonte1> Já possui login? </Fonte1>  
+                        </DivGap2>
+                    <DivGap2>
+                        <Botao> Cadastrar </Botao>
+                        <Botao onClick={() => navigate("/login")}> Logue aqui!!!</Botao>
+                    </DivGap2>  
+                </DivGap>   
+                <Divimg> <Doctorimg src={ ImagemMedico } alt="Imagem Médico" /> </Divimg>    
+            </DivFrame>
+        </Divall>
+    )
+
 }
 
-export default Cadastro;
+export default Cadastro
